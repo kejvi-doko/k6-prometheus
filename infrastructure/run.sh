9b8c6b268477d826d33132d946ae7f4a3a45500a
Kejvi Doko <kejvidoko@Kejvis-MacBook-Pro-2.local>
2025-10-30 12:38:46 +0100

    update structure

--- infrastructure/run.sh
+++ infrastructure/run.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 set -e
-kubectl apply -f namespaces.yaml
 
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
+kubectl -n k6-operator delete testrun timesheets-load --ignore-not-found
 
-helm upgrade --install kps prometheus-community/kube-prometheus-stack \
-  -n monitoring \
-  -f helm/prometheus/values.yaml
-
-helm upgrade --install grafana grafana/grafana \
-  -n monitoring \
-  -f helm/grafana/values.yaml
-
-helm upgrade --install k6-operator grafana/k6-operator \
-  -n k6-operator \
-  -f helm/k6-operator/values.yaml
+BUILD_TAG=demo-002 envsubst < testrun-timesheets.yaml | kubectl apply -f -
