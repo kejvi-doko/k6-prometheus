15325a3708ffb580b83c30bcaa0d351c0f949db3
Kejvi Doko <kejvidoko@gmail.com>
2025-11-03 15:30:50 +0100

    feat: update testrun template

--- infrastructure/run.sh
+++ infrastructure/run.sh
@@ -3,4 +3,16 @@ set -e
 
 kubectl -n k6-operator delete testrun timesheets-load --ignore-not-found
 
-BUILD_TAG=demo-002 envsubst < testrun-timesheets.yaml | kubectl apply -f -
+# Usage: ./run.sh [BUILD_TAG]
+# If BUILD_TAG is not provided, generate a random one.
+BUILD_TAG_INPUT="$1"
+if [ -z "$BUILD_TAG_INPUT" ]; then
+  # random tag: build-YYYYmmdd-HHMMSS-<4hex>
+  BUILD_TAG="build-$(date +%Y%m%d-%H%M%S)-$(openssl rand -hex 2 2>/dev/null || echo rnd)"
+else
+  BUILD_TAG="$BUILD_TAG_INPUT"
+fi
+
+echo "Using BUILD_TAG=${BUILD_TAG}"
+
+BUILD_TAG="$BUILD_TAG" envsubst < testrun-timesheets.yaml | kubectl apply -f -
